f5b6e5d108fea91061eb52ca27e9991124710f94
Melinda McDowell <melinda.e.mcdowell@outlook.com>
2022-04-07 22:50:18 -0700

    refactored formulas

--- brain_game.py
+++ brain_game.py
@@ -2,24 +2,7 @@
 
 """ Game to determine if your brain is ok from this pandemic """
 
-
-# questions and answers
-# DONE: make dict
-q1 = "When you meet somebody new, how do you greet them?"
-q1a = "Hello!" # 2
-q1b = "Goodbye!" # 1
-q1c = "Potato!" # 0
-
-q2 = "A friend has invited you to brunch; how do you respond?"
-q2a = 'Say "Sure", knowing you absolutely will not show up.' # 1
-q2b = 'Say "That will be lovely", then go to brunch.' # 2
-q2c = "Hide in your closet." # 0
-
-q3 = "What kind of pants are you wearing?" 
-q3a = "Pajama" # 1
-q3b = "Slacks or Jeans" # 2
-q3c = "None" # 0
-
+# questions dictionary with question, choices, and scores
 questions = {
     "q1": 
     {"question": "When you meet somebody new, how do you greet them?", 
@@ -46,51 +29,44 @@ questions = {
             }
     }
             }
+
 # build question format
-def questionbuilder(qno) :
+def askquestion(qno) :
     q = questions[qno]["question"]
     a = questions[qno]["choice"]["a"]["answer"]
     b = questions[qno]["choice"]["b"]["answer"]
     c = questions[qno]["choice"]["c"]["answer"]
 
-    question = input(f"{q}\nYour choices are: \nA: {a} \nB: {b} \nC: {c} \nYour Answer ->  ").lower()
+    question = input(f"\n{q}\nYour choices are: \nA: {a} \nB: {b} \nC: {c} \nYour Answer ->  ").lower()
 
     return question
 
-# ask questions, each answer associates ok or not ok brain
-def askquestions() :        
-    q1response = questionbuilder("q1")
-    q2response = questionbuilder("q2")
-    q3response = questionbuilder("q3")
+# calculate brain_meter by getting score based on response
+def calculatemeter(qno, brain_meter):
+    response = ""
 
-    # brain meter
+    while response == "" or response not in questions[qno]["choice"]:
+        response = askquestion(qno)
+        if response in questions[qno]["choice"]:
+            brain_meter += questions[qno]["choice"][response]["score"]
+            break            
+        else:
+            print("\nYour brain is not ok. Try again") 
+
+    return brain_meter
+
+# tell user how their brain scored
+def isyourbrainok() :  
+    
     brain_meter = 0
 
-    # calculate brain_meter
-    while q1response != "c":
-        if q1response == "a":
-            brain_meter += 2
-            break
-        else: 
-            brain_meter += 1
-            break
+    # run for each question, redefine brain_meter after each
+    brain_meter = calculatemeter("q1", brain_meter)
+    brain_meter = calculatemeter("q2", brain_meter)
+    brain_meter = calculatemeter("q3", brain_meter)
 
-    while q2response != "c":
-        if q2response == "a":
-            brain_meter += 1
-            break
-        else:
-            brain_meter += 2
-            break
+    print()
 
-    while q3response != "c":
-        if q3response == "a":
-            brain_meter += 1
-            break
-        else:
-            brain_meter += 2
-            break
-    
     if brain_meter == 0:
         print("Your brain is definitely not ok.")
     elif brain_meter == 6:
@@ -99,6 +75,6 @@ def askquestions() :
         print("You need a little brain healing")
 
 def main() :
-    askquestions()
+    isyourbrainok()
 
 main()
